b4edf78a7c033a0b3128567ab2229df081ca4c38
Mo Zhou <cdluminate@gmail.com>
2018-04-27 14:53:47 +0000

    control{,.py}: update

--- debian/control.py
+++ debian/control.py
@@ -2,8 +2,72 @@
 # Helper Script for INTEL-MKL Packaging
 # Copyright (C) 2018 Mo Zhou <cdluminate@gmail.com>
 # MIT License
-import os, re, sys, subprocess
+import os, re, sys, subprocess, glob
 from typing import *
+
+
+def getDpkgArchitecture(query: str) -> str:
+    '''
+    dpkg-architecture -qQUERY
+    '''
+    result = subprocess.Popen(['dpkg-architecture', f'-q{query}'],
+             stdout=subprocess.PIPE).communicate()[0].decode().strip()
+    return result
+
+
+def installFile(fpath: str, package: str, dest: str = '') -> None:
+    '''
+    Write an entry in specified package's install file, for installing
+    the file to dest.
+    '''
+    with open(os.path.join('debian', f'{package}.install'), 'a') as f:
+        f.write(f'{fpath}\t{dest}\n' if dest else f'{fpath}')
+
+
+def installSharedObjects() -> None:
+    '''
+    Glob all the shared object and write install entries for them
+    '''
+    # Glob libs
+    shlibs = glob.glob('opt/**/**.so', recursive=True)
+    # filter the lib list by architecture
+    deb_host_arch = getDpkgArchitecture('DEB_HOST_ARCH')
+    deb_host_multiarch = getDpkgArchitecture('DEB_HOST_MULTIARCH')
+    if deb_host_arch == 'amd64':
+        shlibs = [x for x in shlibs if 'ia32' not in x]
+        # dedup
+        shlibs = [x for x in shlibs if 'intel64_lin' not in x]
+    else:
+        shlibs = [x for x in shlibs if 'intel64' not in x]
+        shlibs = [x for x in shlibs if 'ia32_lin' not in x]
+    # don't care about the libs used by intel's installer
+    shlibs = [x for x in shlibs if 'uninstall' not in x]
+    # tbb and iomp5 are already provided by other packages
+    shlibs = [x for x in shlibs if 'iomp' not in x]
+    shlibs = [x for x in shlibs if 'tbb/' not in x]
+    # get package names in control file
+    packages = parsePackages()
+    # now let's install them !
+    for so in shlibs:
+        path, fname = os.path.dirname(so), os.path.basename(so)
+        package = re.sub('\.so$', '', fname).replace('_', '-')
+        #print(path, fname, package)
+        if package in packages:
+            print(f'installing {fname} ➜ {package}')
+            installFile(os.path.join(path, fname), package,
+                    f'usr/lib/{deb_host_multiarch}/')
+        else:
+            print(f'Warning: Which package should ship {fname} ??')
+
+
+def parsePackages() -> List[str]:
+    '''
+    Parse debian/control and return a list of Package names
+    '''
+    with open('debian/control', 'r') as f:
+        control = f.read()
+    packages = re.findall('Package:\s*(.*)', control)
+    return packages
 
 
 def parse_binary_packages(control: str) -> List[Tuple[str,str]]:
@@ -29,11 +93,9 @@ def generate_install_files(packages: List[str], multiarch: str) -> None:
 
 
 if __name__ == '__main__':
-    deb_host_arch = subprocess.Popen(['dpkg-architecture', '-qDEB_HOST_ARCH'],
-                    stdout=subprocess.PIPE).communicate()[0].decode().strip()
-    deb_host_multiarch = subprocess.Popen(['dpkg-architecture',
-                    '-qDEB_HOST_MULTIARCH'], stdout=subprocess.PIPE
-                     ).communicate()[0].decode().strip()
+
+    installSharedObjects()
+    exit()
 
     # Read control file and parse
     control = open('debian/control', 'r').read()
